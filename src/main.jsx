27e15eb2f2d1cf7ad59d6e9515c8583b69ce6906
samya-roy <samyaroy999@gmail.com>
2024-03-25 00:28:42 +0600

    Route is rady in project

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import Root from './components/Root/Root'
+import Home from './components/Home/Home'
+import Blog from './components/Blog/Blog'
+import Jobs from './components/Jobs/Jobs'
+import Error from './components/Error/Error'
 
 const router = createBrowserRouter([
+
   {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+    path: '/',
+    element: <Root/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path:'/',
+        element: <Home/>,
+      },
+      {
+        path: '/jobs',
+        element: <Jobs/>,
+      },
+      {
+        path: '/blog',
+        element: <Blog/>,
+      },
+    ]
+  }
+
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
